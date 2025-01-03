bd5710b277f5c23551a953f3efa60200ead4523f
Yana Lunina <yanalunina96@gmail.com>
2025-01-03 22:16:51 +0200

    add content & styles

--- js/services-products.js
+++ js/services-products.js
@@ -1,37 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("../data/games-data.json")
+  fetch("../data/services-data.json")
     .then((response) => response.json())
     .then((data) => {
-      let gamesData = data.games;
-      const gamesList = document.getElementById("games-products");
+      let servicesData = data.services;
+      const servicesList = document.getElementById("services-products");
 
-      if (!gamesList) {
+      if (!servicesList) {
         return;
       }
 
       function displayProducts() {
-        gamesList.innerHTML = "";
-        gamesData.forEach((game) => {
+        servicesList.innerHTML = "";
+        servicesData.forEach((service) => {
           const li = document.createElement("li");
           li.classList.add("card-item");
 
           li.innerHTML = `
             <div class="card-thumb">
-              <img src="../${game.photo}" alt="${game.title}" width="370"
+              <img src="../${service.photo}" alt="${service.title}" width="370"
                   class="card-img">
               <div class="card-overlay">
                 <p class="bg-text" >
-                ${game.description}
+                ${service.description}
                 </p>
               </div>
             </div>
             <div class="card-box">
-                <h2 class="card-title">${game.title}</h2>
-                <p class="card-text">${game.type}</p>
-                <p class="card-text price">${game.price}</p>
+                <h2 class="card-title">${service.title}</h2>
+                <p class="card-text">${service.type}</p>
+                <p class="card-text price">${service.price}</p>
             </div>
             `;
-          gamesList.appendChild(li);
+          servicesList.appendChild(li);
         });
       }
       displayProducts();
